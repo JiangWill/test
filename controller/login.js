52613c8cc1ac46a34398c8ff8e3b98523f49e668
wj <944064720@qq.com>
2019-05-09 18:10:06 +0800

    1

--- controller/login.js
+++ controller/login.js
@@ -1,25 +1,7 @@
-var fn_index = async (ctx, next) => {
-   ctx.response.body = `<h1>Index</h1>
-       <form action="/signin" method="post">
-           <p>Name: <input name="name" value="koa"></p>
-           <p>Password: <input name="password" type="password"></p>
-           <p><input type="submit" value="Submit"></p>
-       </form>`;
-};
+const Koa = require('koa')
+const app = new Koa()
 
-var fn_signin = async (ctx, next) => {
-   var name = ctx.request.body.name || '',
-       password = ctx.request.body.password || '';
-   console.log(`signin with name: ${name}, password: ${password}`);
-   if (name === 'koa' && password === '12345') {
-       ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
-   } else {
-       ctx.response.body = `<h1>Login failed!</h1>
-       <p><a href="/">Try again</a></p>`;
-   }
-};
-
-module.exports = {
-   'GET /': fn_index,
-   'POST /signin': fn_signin
-};
+app.use(async (ctx,next)=>{
+    ctx.body = 'hello 1111'
+})
+app.listen(3000)
